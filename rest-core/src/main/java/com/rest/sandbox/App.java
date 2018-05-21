5b04cabb169c92d45b7c1349ba34ba3b62411ced
egboldyr <egboldyr>
2018-05-21 15:33:27 +0300

    Initial commit

---
+++ rest-core/src/main/java/com/rest/sandbox/App.java
@@ -0,0 +1,16 @@
+package com.rest.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by EGBoldyr on 21.05.18.
+ */
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
