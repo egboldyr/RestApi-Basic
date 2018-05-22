a2f018f80f3ec4dc09dd54cbd5739091a00f8691
egboldyr <egboldyr>
2018-05-22 10:25:28 +0300

    Endpoint Update

---
+++ rest-api/src/main/java/com/rest/api/response/ResponseCode.java
@@ -0,0 +1,9 @@
+package com.rest.api.response;
+
+/**
+ * Created by EGBoldyr on 22.05.18.
+ */
+public class ResponseCode {
+
+    public static final String OK = "200";
+}
--- rest-core/src/main/java/com/rest/sandbox/endpoint/SandboxRestEndpoint.java
+++ rest-core/src/main/java/com/rest/sandbox/endpoint/SandboxRestEndpoint.java
@@ -5,6 +5,9 @@ import com.rest.api.request.GeneralRequest;
 import com.rest.api.request.sandbox.CreateSandboxParameters;
 import com.rest.api.response.GeneralResponse;
 import com.rest.api.dto.SandboxDTO;
+import com.rest.api.response.ResponseCode;
+import com.rest.sandbox.service.SandboxService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,18 +20,21 @@ import java.util.List;
 @RestController
 public class SandboxRestEndpoint implements SandboxEndpoint {
 
+    @Autowired
+    private SandboxService service;
+
     @Override
     public GeneralResponse<List<SandboxDTO>> testGetMethod() {
-        return new GeneralResponse<List<SandboxDTO>>(null, null);
+        return new GeneralResponse<List<SandboxDTO>>(ResponseCode.OK, service.findAll());
     }
 
     @Override
     public GeneralResponse<SandboxDTO> testGetMethod(Long id) {
-        return null;
+        return new GeneralResponse<>(ResponseCode.OK, service.findById(id));
     }
 
     @Override
     public GeneralResponse<Long> testPostMethod(@RequestBody GeneralRequest<CreateSandboxParameters> request) {
-        return new GeneralResponse<Long>(null, null);
+        return new GeneralResponse<Long>(ResponseCode.OK, service.create(request.getParameters()));
     }
 }
